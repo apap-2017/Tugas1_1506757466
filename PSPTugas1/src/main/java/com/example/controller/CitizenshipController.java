8963e26b4be698013f8e9db92c21d072bef29ea7
aninditowibi24 <aninditowibi24@gmail.com>
2017-10-22 17:06:18 +0700

    add writeup; add fragmented css

--- PSPTugas1/src/main/java/com/example/controller/CitizenshipController.java
+++ PSPTugas1/src/main/java/com/example/controller/CitizenshipController.java
@@ -3,15 +3,14 @@ package com.example.controller;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-import org.apache.catalina.valves.StuckThreadDetectionValve;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
